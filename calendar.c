f92a557e4c2a9094e2d4fa3598f62175f5da6f99
Sam Huang <63796156+k612created@users.noreply.github.com>
2021-01-08 12:11:30 +0800

    Update calendar.c

--- calendar.c
+++ calendar.c
@@ -1,1 +1,12 @@
-
+#ifndef __K612_GROCERIES_CALENDAR__
+#define __K612_GROCERIES_CALENDAR__
+#endif
+#include <stdio.h>
+const char * Weekdays[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+char * GetWeekdays(int Year, int Month, int Day) {
+	// This function returns weekdays in string.
+	// @Arguments: Year: YYYY, Month: M/MM and Day: D/DD.
+	int m = (Month <= 2) ? Month + 12 : Month;
+	int y = (m > 12) ? Year - 1 : Year;
+	return Weekdays[(Day + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400)]; // Using the formula. w=(d+2*m+3*(m+1)/5+y+y/4-y/100+y/400
+}
